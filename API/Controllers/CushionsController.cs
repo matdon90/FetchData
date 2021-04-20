7c63885e690788721c50f74df2bc09707a5e453f
matdon90 <mdonhefner@gmail.com>
2021-04-20 15:01:39 +0200

    Adds GetSingleCushion action to cushions controller.

--- API/Controllers/CushionsController.cs
+++ API/Controllers/CushionsController.cs
@@ -1,18 +1,27 @@
 ﻿using Application.Cushions.Queries;
 using Application.Cushions.Queries.ListCushion;
+using Application.Cushions.Queries.SingleCushion;
+using Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-
     public class CushionsController : ApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<CushionDto>>> GetList()
+        public async Task<ActionResult<List<CushionDto>>> GetCushionsList()
         {
             return Ok(await Mediator.Send(new ListCushionQuery()));
+        }
+
+        [HttpGet("{stand}/{position}")]
+        public async Task<ActionResult<CushionDto>> GetCushionSingle(int stand, string position)
+        {
+            var rollPosition = (RollPosition)Enum.Parse(typeof(RollPosition), position, true);
+            return Ok(await Mediator.Send(new SingleCushionQuery(stand, rollPosition)));
         }
     }
 }
