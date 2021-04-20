4ddd533f152f69c9936e5e5d4adda0e0225e8591
matdon90 <mdonhefner@gmail.com>
2021-04-20 13:47:21 +0200

    Adds single cushion query and query handler.

---
+++ Application/Cushions/Queries/SingleCushion/SingleCushionQuery.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+using MediatR;
+
+namespace Application.Cushions.Queries.SingleCushion
+{
+    public class SingleCushionQuery : IRequest<CushionDto>
+    {
+        public int Stand { get; set; }
+        public RollPosition RollPosition { get; set; }
+
+        public SingleCushionQuery()
+        {
+
+        }
+        public SingleCushionQuery(int stand, RollPosition rollPosition)
+        {
+            this.Stand = stand;
+            this.RollPosition = rollPosition;
+        }
+    }
+}
---
+++ Application/Cushions/Queries/SingleCushion/SingleCushionQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using Application.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Cushions.Queries.SingleCushion
+{
+    public class SingleCushionQueryHandler : IRequestHandler<SingleCushionQuery, CushionDto>
+    {
+        private readonly IOracleDbContext _oracleDbContext;
+        private readonly IPostgresDbContext _postgresDbContext;
+
+        public SingleCushionQueryHandler(
+            IOracleDbContext oracleDbContext,
+            IPostgresDbContext postgresDbContext)
+        {
+            _oracleDbContext = oracleDbContext;
+            _postgresDbContext = postgresDbContext;
+        }
+
+        public async Task<CushionDto> Handle(SingleCushionQuery request, CancellationToken cancellationToken)
+        {
+            var workRoll = await _postgresDbContext.WorkRolls
+                .FirstOrDefaultAsync(w => w.StandNumber == request.Stand
+                                          && w.Position == request.RollPosition);
+
+            var cushionsSet = await _oracleDbContext.CushionsSets
+                .FirstOrDefaultAsync(c => c.RollNumber == workRoll.RollNumber);
+
+            return new CushionDto()
+            {
+                StandNumber = workRoll.StandNumber,
+                Position = workRoll.Position,
+                CushionSetNumber = cushionsSet.CushionSetNumber
+            };
+        }
+    }
+}
