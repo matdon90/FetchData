7f1afa120f9de127f6c5829913ca71041af3c2d8
matdon90 <mdonhefner@gmail.com>
2021-04-23 12:02:30 +0200

    Adds settings for oracle DB in version 11g EE. Creates initial migration.

---
+++ Persistence/Migrations/20210423100140_OracleDbInitialMigration.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class OracleDbInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "LOGMNR_EVOLVE_SEQ$");
+
+            migrationBuilder.CreateSequence(
+                name: "LOGMNR_SEQ$");
+
+            migrationBuilder.CreateSequence(
+                name: "LOGMNR_UIDS$");
+
+            migrationBuilder.CreateSequence(
+                name: "MVIEW$_ADVSEQ_GENERIC");
+
+            migrationBuilder.CreateSequence(
+                name: "MVIEW$_ADVSEQ_ID");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_EXCEPTIONS_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_FLAVOR_NAME_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_FLAVORS_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_REFRESH_TEMPLATES_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_REPPROP_KEY");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_RUNTIME_PARMS_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_TEMP_OUTPUT_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_TEMPLATE_OBJECTS_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_TEMPLATE_PARMS_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_TEMPLATE_REFGROUPS_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_TEMPLATE_SITES_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_USER_AUTHORIZATIONS_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT$_USER_PARM_VALUES_S");
+
+            migrationBuilder.CreateSequence(
+                name: "REPCAT_LOG_SEQUENCE");
+
+            migrationBuilder.CreateSequence(
+                name: "TEMPLATE$_TARGETS_S");
+
+            migrationBuilder.CreateTable(
+                name: "CUSHIONSSETS",
+                columns: table => new
+                {
+                    CUSHIONSETID = table.Column<int>(nullable: false),
+                    ROLLNUMBER = table.Column<int>(nullable: false),
+                    CUSHIONSETNUMBER = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("CUSHIONSSETS_PK", x => x.CUSHIONSETID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CUSHIONSSETS");
+
+            migrationBuilder.DropSequence(
+                name: "LOGMNR_EVOLVE_SEQ$");
+
+            migrationBuilder.DropSequence(
+                name: "LOGMNR_SEQ$");
+
+            migrationBuilder.DropSequence(
+                name: "LOGMNR_UIDS$");
+
+            migrationBuilder.DropSequence(
+                name: "MVIEW$_ADVSEQ_GENERIC");
+
+            migrationBuilder.DropSequence(
+                name: "MVIEW$_ADVSEQ_ID");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_EXCEPTIONS_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_FLAVOR_NAME_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_FLAVORS_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_REFRESH_TEMPLATES_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_REPPROP_KEY");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_RUNTIME_PARMS_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_TEMP_OUTPUT_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_TEMPLATE_OBJECTS_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_TEMPLATE_PARMS_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_TEMPLATE_REFGROUPS_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_TEMPLATE_SITES_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_USER_AUTHORIZATIONS_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT$_USER_PARM_VALUES_S");
+
+            migrationBuilder.DropSequence(
+                name: "REPCAT_LOG_SEQUENCE");
+
+            migrationBuilder.DropSequence(
+                name: "TEMPLATE$_TARGETS_S");
+        }
+    }
+}
--- Persistence/OracleDbContext.cs
+++ Persistence/OracleDbContext.cs
@@ -4,12 +4,77 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class OracleDbContext : DbContext, IOracleDbContext
+    public partial class OracleDbContext : DbContext, IOracleDbContext
     {
         public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
         {
         }
 
         public DbSet<CushionSet> CushionsSets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CushionSet>(entity =>
+            {
+                entity.HasKey(e => e.CushionSetId)
+                    .HasName("CUSHIONSSETS_PK");
+
+                entity.ToTable("CUSHIONSSETS");
+
+                entity.Property(e => e.CushionSetId)
+                    .HasColumnName("CUSHIONSETID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CushionSetNumber)
+                    .IsRequired()
+                    .HasColumnName("CUSHIONSETNUMBER");
+
+                entity.Property(e => e.RollNumber).HasColumnName("ROLLNUMBER");
+            });
+
+            modelBuilder.HasSequence("LOGMNR_EVOLVE_SEQ$");
+
+            modelBuilder.HasSequence("LOGMNR_SEQ$");
+
+            modelBuilder.HasSequence("LOGMNR_UIDS$");
+
+            modelBuilder.HasSequence("MVIEW$_ADVSEQ_GENERIC");
+
+            modelBuilder.HasSequence("MVIEW$_ADVSEQ_ID");
+
+            modelBuilder.HasSequence("REPCAT$_EXCEPTIONS_S");
+
+            modelBuilder.HasSequence("REPCAT$_FLAVOR_NAME_S");
+
+            modelBuilder.HasSequence("REPCAT$_FLAVORS_S");
+
+            modelBuilder.HasSequence("REPCAT$_REFRESH_TEMPLATES_S");
+
+            modelBuilder.HasSequence("REPCAT$_REPPROP_KEY");
+
+            modelBuilder.HasSequence("REPCAT$_RUNTIME_PARMS_S");
+
+            modelBuilder.HasSequence("REPCAT$_TEMP_OUTPUT_S");
+
+            modelBuilder.HasSequence("REPCAT$_TEMPLATE_OBJECTS_S");
+
+            modelBuilder.HasSequence("REPCAT$_TEMPLATE_PARMS_S");
+
+            modelBuilder.HasSequence("REPCAT$_TEMPLATE_REFGROUPS_S");
+
+            modelBuilder.HasSequence("REPCAT$_TEMPLATE_SITES_S");
+
+            modelBuilder.HasSequence("REPCAT$_USER_AUTHORIZATIONS_S");
+
+            modelBuilder.HasSequence("REPCAT$_USER_PARM_VALUES_S");
+
+            modelBuilder.HasSequence("REPCAT_LOG_SEQUENCE");
+
+            modelBuilder.HasSequence("TEMPLATE$_TARGETS_S");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
